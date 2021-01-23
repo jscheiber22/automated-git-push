f3fc7e74cf4268bbb3e98ea89737126b1862e692
James Scheiber <jscheiber22@gmail.com>
2021-01-23 14:40:18 -0800

    added initial pulling

--- main.py
+++ main.py
@@ -8,10 +8,18 @@ if __name__ == "__main__":
         path = sys.argv[1]
     except IndexError:
         print("Defaulting and making you wait because you're lazy smh")
-        sleep(5)
+        sleep(2)
         path = "/home/james/Documents/Programming/"
     except:
         raise
+
+    print("Pulling all directories first.\n")
+    for dir in os.listdir(path):
+        os.chdir(path + dir)
+        pwd = str(subprocess.check_output(["pwd"]))
+        pwd = pwd.replace(path, "").replace("b'", "").replace("\\n'", "")
+        print("\nPulling " + str(pwd))
+        subprocess.call(["git", "pull"])
 
     for dir in os.listdir(path):
         try:
